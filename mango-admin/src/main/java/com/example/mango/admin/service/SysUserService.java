40badd29852f892a940a3fcfe125c3ad1aaa00b4
genleel <imligen@icloud.com>
2022-11-15 14:10:55 +0800

    page helper

--- mango-admin/src/main/java/com/example/mango/admin/service/SysUserService.java
+++ mango-admin/src/main/java/com/example/mango/admin/service/SysUserService.java
@@ -1,15 +1,18 @@
 package com.example.mango.admin.service;
 
 import com.example.mango.admin.model.SysUser;
+import com.example.mango.core.service.CurdService;
 
 import java.util.List;
 
 
-public interface SysUserService {
+public interface SysUserService extends CurdService<SysUser> {
     /**
      * 查询全部用户
      * @return
      */
     List<SysUser> findAll();
+
+
 
 }
--- mango-admin/src/main/java/com/example/mango/admin/service/impl/SysUserServiceImpl.java
+++ mango-admin/src/main/java/com/example/mango/admin/service/impl/SysUserServiceImpl.java
@@ -2,8 +2,13 @@ package com.example.mango.admin.service.impl;
 
 import com.example.mango.admin.dao.SysUserMapper;
 import com.example.mango.admin.model.SysUser;
+import com.example.mango.admin.model.SysUserRole;
 import com.example.mango.admin.service.SysUserService;
+import com.example.mango.core.page.MybatisPageHelper;
+import com.example.mango.core.page.PageRequest;
+import com.example.mango.core.page.PageResult;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -16,5 +21,31 @@ public class SysUserServiceImpl implements SysUserService {
     @Override
     public List<SysUser> findAll() {
         return sysUserMapper.findAll();
+    }
+
+
+    @Override
+    public int save(SysUser record) {
+        return 0;
+    }
+
+    @Override
+    public int delete(SysUser record) {
+        return 0;
+    }
+
+    @Override
+    public int delete(List<SysUser> records) {
+        return 0;
+    }
+
+    @Override
+    public SysUser findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public PageResult findPage(PageRequest pageRequest) {
+        return MybatisPageHelper.findPage(pageRequest, sysUserMapper);
     }
 }
