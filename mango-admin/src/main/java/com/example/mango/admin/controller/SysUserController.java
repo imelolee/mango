7130cd47fb4fa7f8a0f4e9091d8574e09913a276
imelolee <wataruri1129@gmail.com>
2022-12-31 17:36:31 +0800

    Update controller

--- mango-admin/src/main/java/com/example/mango/admin/controller/SysUserController.java
+++ mango-admin/src/main/java/com/example/mango/admin/controller/SysUserController.java
@@ -1,11 +1,25 @@
 package com.example.mango.admin.controller;
 
+import com.example.mango.admin.constant.SysConstants;
+import com.example.mango.admin.model.SysUser;
 import com.example.mango.admin.service.SysUserService;
+import com.example.mango.admin.util.PasswordUtils;
+import com.example.mango.common.utils.FileUtils;
 import com.example.mango.core.http.HttpResult;
 import com.example.mango.core.page.PageRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.util.List;
+
+
+/**
+ * 用户控制器
+ * @author Louis
+ * @date Jan 13, 2019
+ */
 @RestController
 @RequestMapping("user")
 public class SysUserController {
@@ -13,13 +27,71 @@ public class SysUserController {
     @Autowired
     private SysUserService sysUserService;
 
-    @GetMapping(value="/findAll")
-    public Object findAll() {
-        return sysUserService.findAll();
+    @PostMapping(value="/save")
+    public HttpResult save(@RequestBody SysUser record) {
+        SysUser user = sysUserService.findById(record.getId());
+        if(user != null) {
+            if(SysConstants.ADMIN.equalsIgnoreCase(user.getName())) {
+                return HttpResult.error("超级管理员不允许修改!");
+            }
+        }
+        if(record.getPassword() != null) {
+            String salt = PasswordUtils.getSalt();
+            if(user == null) {
+                // 新增用户
+                if(sysUserService.findByName(record.getName()) != null) {
+                    return HttpResult.error("用户名已存在!");
+                }
+                String password = PasswordUtils.encode(record.getPassword(), salt);
+                record.setSalt(salt);
+                record.setPassword(password);
+            } else {
+                // 修改用户, 且修改了密码
+                if(!record.getPassword().equals(user.getPassword())) {
+                    String password = PasswordUtils.encode(record.getPassword(), salt);
+                    record.setSalt(salt);
+                    record.setPassword(password);
+                }
+            }
+        }
+        return HttpResult.ok(sysUserService.save(record));
     }
 
-    @PostMapping(value = "/findPage")
-    public HttpResult findPage(@RequestBody PageRequest pageRequest){
+    @PostMapping(value="/delete")
+    public HttpResult delete(@RequestBody List<SysUser> records) {
+        for(SysUser record:records) {
+            SysUser sysUser = sysUserService.findById(record.getId());
+            if(sysUser != null && SysConstants.ADMIN.equalsIgnoreCase(sysUser.getName())) {
+                return HttpResult.error("超级管理员不允许删除!");
+            }
+        }
+        return HttpResult.ok(sysUserService.delete(records));
+    }
+
+    @GetMapping(value="/findByName")
+    public HttpResult findByName(@RequestParam String name) {
+        return HttpResult.ok(sysUserService.findByName(name));
+    }
+
+    @GetMapping(value="/findPermissions")
+    public HttpResult findPermissions(@RequestParam String name) {
+        return HttpResult.ok(sysUserService.findPermissions(name));
+    }
+
+    @GetMapping(value="/findUserRoles")
+    public HttpResult findUserRoles(@RequestParam Long userId) {
+        return HttpResult.ok(sysUserService.findUserRoles(userId));
+    }
+
+    @PostMapping(value="/findPage")
+    public HttpResult findPage(@RequestBody PageRequest pageRequest) {
         return HttpResult.ok(sysUserService.findPage(pageRequest));
     }
+
+    @PostMapping(value="/exportExcelUser")
+    public void exportExcelUser(@RequestBody PageRequest pageRequest, HttpServletResponse res) {
+        File file = sysUserService.createUserExcelFile(pageRequest);
+        FileUtils.downloadFile(res, file, file.getName());
+    }
+
 }
