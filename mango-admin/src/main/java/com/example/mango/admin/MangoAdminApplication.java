37602db9482361c97dc8d90520147bb7d32be64f
genleel <meloligen@gmail.com>
2023-01-14 10:57:41 +0800

    Add consul

--- mango-admin/src/main/java/com/example/mango/admin/MangoAdminApplication.java
+++ mango-admin/src/main/java/com/example/mango/admin/MangoAdminApplication.java
@@ -3,8 +3,10 @@ package com.example.mango.admin;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication(scanBasePackages = {"com.example.mango.admin"})
+@EnableDiscoveryClient
+@SpringBootApplication(scanBasePackages = {"com.example.mango"})
 @MapperScan("com.example.mango.admin.dao")
 public class MangoAdminApplication {
 
