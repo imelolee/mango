c5fc22fde411950e426016d1642edb0457e6b230
imelolee <wataruri1129@gmail.com>
2023-01-16 11:17:07 +0800

    Add LoadBalancerClient

---
+++ mango-producer2/src/main/java/com/example/mango/producer/MangoProducer2Application.java
@@ -0,0 +1,15 @@
+package com.example.mango.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MangoProducer2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MangoProducer2Application.class, args);
+    }
+
+}
